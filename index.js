867af60b43386d00760a2ea2facb8b4e1549d188
zaktaw <zaktaw@gmail.com>
2021-02-12 15:23:22 +0100

    Bot can now recieve and handle arguments from Discord server

--- index.js
+++ index.js
@@ -8,3 +8,31 @@ bot.on('ready', () => {
 }); 
 
 bot.login(config.token);
+
+bot.on('message', (msg) => {
+    
+    let args = msg.content.substring(PREFIX.length).split(" ");
+
+    // Prevent spam from bot
+    if (msg.author.bot) return; // stops bot from replying to itself
+    if (!msg.guild) return; // bot will only reply if message is sent in the guild (server)
+    if (!msg.content.startsWith(PREFIX)) return; // bot will only reply if the message starts with the specified prefix
+
+    // Only admins can use the bot
+    if (!msg.member.hasPermission('ADMINISTRATOR')) {
+        msg.channel.send("You have to be an administrator to use this bot");
+        return;
+    }
+
+    // Handle arguments given
+    switch (args[0].toLowerCase()) {
+
+        case 'test' :
+            msg.channel.send("This bot is working");
+            break;
+
+        default :
+            msg.channel.send(`"${args[0]}" is an invalid command`);
+
+    }   
+});
